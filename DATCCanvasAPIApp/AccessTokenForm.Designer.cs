b5025eb361fc1e19b2c9b36cc011698cf753fdb9
RayEG <george.ray@davistech.edu>
2020-05-29 12:17:54 -0600

    Updated Icon

--- DATCCanvasAPIApp/AccessTokenForm.Designer.cs
+++ DATCCanvasAPIApp/AccessTokenForm.Designer.cs
@@ -42,9 +42,10 @@
             // 
             // saveAccessToken
             // 
-            this.saveAccessToken.Location = new System.Drawing.Point(12, 241);
+            this.saveAccessToken.Location = new System.Drawing.Point(24, 463);
+            this.saveAccessToken.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.saveAccessToken.Name = "saveAccessToken";
-            this.saveAccessToken.Size = new System.Drawing.Size(136, 32);
+            this.saveAccessToken.Size = new System.Drawing.Size(272, 62);
             this.saveAccessToken.TabIndex = 1;
             this.saveAccessToken.Text = "Save Access Token";
             this.saveAccessToken.UseVisualStyleBackColor = true;
@@ -52,18 +53,20 @@
             // 
             // txbCurrentAccessToken
             // 
-            this.txbCurrentAccessToken.Location = new System.Drawing.Point(12, 213);
+            this.txbCurrentAccessToken.Location = new System.Drawing.Point(24, 410);
+            this.txbCurrentAccessToken.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.txbCurrentAccessToken.Name = "txbCurrentAccessToken";
-            this.txbCurrentAccessToken.Size = new System.Drawing.Size(370, 20);
+            this.txbCurrentAccessToken.Size = new System.Drawing.Size(736, 31);
             this.txbCurrentAccessToken.TabIndex = 2;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(128, 181);
+            this.label2.Location = new System.Drawing.Point(256, 348);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(109, 20);
+            this.label2.Size = new System.Drawing.Size(218, 37);
             this.label2.TabIndex = 4;
             this.label2.Text = "Access Token";
             // 
@@ -71,24 +74,27 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(128, 72);
+            this.label3.Location = new System.Drawing.Point(256, 138);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(148, 20);
+            this.label3.Size = new System.Drawing.Size(292, 37);
             this.label3.TabIndex = 5;
             this.label3.Text = "Instructure Website";
             // 
             // txbWebsite
             // 
-            this.txbWebsite.Location = new System.Drawing.Point(12, 103);
+            this.txbWebsite.Location = new System.Drawing.Point(24, 198);
+            this.txbWebsite.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.txbWebsite.Name = "txbWebsite";
-            this.txbWebsite.Size = new System.Drawing.Size(370, 20);
+            this.txbWebsite.Size = new System.Drawing.Size(736, 31);
             this.txbWebsite.TabIndex = 6;
             // 
             // btnSaveWebsite
             // 
-            this.btnSaveWebsite.Location = new System.Drawing.Point(12, 129);
+            this.btnSaveWebsite.Location = new System.Drawing.Point(24, 248);
+            this.btnSaveWebsite.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.btnSaveWebsite.Name = "btnSaveWebsite";
-            this.btnSaveWebsite.Size = new System.Drawing.Size(136, 34);
+            this.btnSaveWebsite.Size = new System.Drawing.Size(272, 65);
             this.btnSaveWebsite.TabIndex = 7;
             this.btnSaveWebsite.Text = "Save Website";
             this.btnSaveWebsite.UseVisualStyleBackColor = true;
@@ -96,9 +102,10 @@
             // 
             // btnCancel
             // 
-            this.btnCancel.Location = new System.Drawing.Point(221, 241);
+            this.btnCancel.Location = new System.Drawing.Point(442, 463);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(161, 34);
+            this.btnCancel.Size = new System.Drawing.Size(322, 65);
             this.btnCancel.TabIndex = 8;
             this.btnCancel.Text = "Cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
@@ -107,26 +114,28 @@
             // lblCurrentUser
             // 
             this.lblCurrentUser.AutoSize = true;
-            this.lblCurrentUser.Location = new System.Drawing.Point(38, 21);
+            this.lblCurrentUser.Location = new System.Drawing.Point(76, 40);
+            this.lblCurrentUser.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.lblCurrentUser.Name = "lblCurrentUser";
-            this.lblCurrentUser.Size = new System.Drawing.Size(69, 13);
+            this.lblCurrentUser.Size = new System.Drawing.Size(140, 25);
             this.lblCurrentUser.TabIndex = 9;
             this.lblCurrentUser.Text = "Current User:";
             // 
             // lbxCurrentUser
             // 
             this.lbxCurrentUser.AutoSize = true;
-            this.lbxCurrentUser.Location = new System.Drawing.Point(129, 21);
+            this.lbxCurrentUser.Location = new System.Drawing.Point(258, 40);
+            this.lbxCurrentUser.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.lbxCurrentUser.Name = "lbxCurrentUser";
-            this.lbxCurrentUser.Size = new System.Drawing.Size(85, 13);
+            this.lbxCurrentUser.Size = new System.Drawing.Size(168, 25);
             this.lbxCurrentUser.TabIndex = 11;
             this.lbxCurrentUser.Text = "No User Loaded";
             // 
             // AccessTokenForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(394, 295);
+            this.ClientSize = new System.Drawing.Size(788, 567);
             this.Controls.Add(this.lbxCurrentUser);
             this.Controls.Add(this.lblCurrentUser);
             this.Controls.Add(this.btnCancel);
@@ -137,6 +146,7 @@
             this.Controls.Add(this.txbCurrentAccessToken);
             this.Controls.Add(this.saveAccessToken);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "AccessTokenForm";
             this.Text = "Access Token";
             this.Load += new System.EventHandler(this.AccessTokenForm_Load);
