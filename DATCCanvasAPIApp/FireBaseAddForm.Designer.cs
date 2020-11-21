6834e92fab98ecf2e72c38a256e544470e1c77c1
RayEG <george.ray@davistech.edu>
2020-11-20 20:25:29 -0700

    Added form to add a MongoDB DataConnection

---
+++ DATCCanvasAPIApp/FireBaseAddForm.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace CanvasAPIApp
+{
+    partial class FireBaseAddForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(100, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Needs Built Out";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(273, 176);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(200, 100);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // FireBaseAddForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(429, 343);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "FireBaseAddForm";
+            this.Text = "FireBaseAddForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    }
+}
---
+++ DATCCanvasAPIApp/MongoDBForm.cs
@@ -0,0 +1,95 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Windows.Forms;
+
+namespace CanvasAPIApp
+{
+    public partial class MongoDBForm : Form
+    {
+        Boolean textChanged = false;
+        string connectionString = "";
+        public MongoDBForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MongoDBForm_Load(object sender, EventArgs e)
+        {            
+            TxbUserName.Text = Properties.Settings.Default.MongoDBUserName;
+            TxbPassword.Text = Properties.Settings.Default.MongoDBPW;
+            txbDefaultDatabase.Text = Properties.Settings.Default.MongoDBDefaultDB;
+            TxbGradingCollecion.Text = Properties.Settings.Default.MongoDBGradingCollection;
+            connectionString = Properties.Settings.Default.MongoDBConnectionString;
+            txbConnection.Text = connectionString.Replace(Properties.Settings.Default.MongoDBPW, "<password>");
+
+        }
+        private void SaveSettings()
+        {
+            Properties.Settings.Default.MongoDBUserName = TxbUserName.Text;
+            Properties.Settings.Default.MongoDBPW = TxbPassword.Text;
+            Properties.Settings.Default.MongoDBDefaultDB = txbDefaultDatabase.Text;
+            Properties.Settings.Default.MongoDBGradingCollection = TxbGradingCollecion.Text;
+            Properties.Settings.Default.MongoDBConnectionString = connectionString;
+            Properties.Settings.Default.Save();
+            btnSave.Enabled = false;
+            rtbOutput.Clear();
+            rtbOutput.AppendText("Settings Saved");
+            textChanged = false;
+        }
+
+        private void btnParseConnection_Click(object sender, EventArgs e)
+        {
+            rtbOutput.Clear();
+            connectionString = txbConnection.Text.Replace("<password>", TxbPassword.Text).Replace("<dbname>", txbDefaultDatabase.Text);
+            try
+            {                
+                var client = new MongoClient(connectionString);
+                var database = client.GetDatabase(txbDefaultDatabase.Text);
+
+                foreach (var collection in database.ListCollectionsAsync().Result.ToListAsync<BsonDocument>().Result)
+                {
+                    rtbOutput.AppendText(collection.ToString());
+                }
+                
+                if(textChanged == true)
+                {
+                    btnSave.Enabled = true;                    
+                }
+               
+            }
+            catch (Exception ex)
+            {
+                rtbOutput.AppendText("Connection Failed");
+            }          
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveSettings();            
+        }
+
+        private void TextChange(object sender, EventArgs e)
+        {
+            textChanged = true;
+        }
+
+        private void CheckSaved(object sender, FormClosingEventArgs e)
+        {
+            if (textChanged)
+            {
+                DialogResult result = MessageBox.Show("Would you like to save your settings?", "Unsaved Settings", MessageBoxButtons.YesNo);
+                switch (result)
+                {
+                    case DialogResult.Yes:
+                        SaveSettings();
+                        break;
+                    case DialogResult.No:
+                        break;                   
+                }
+            }
+
+        }
+    }
+}
