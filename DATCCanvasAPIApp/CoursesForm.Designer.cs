0482c3599b480b92c4a36741f3f48cae66524edb
Alexander poop <cheeseonamonkey@gmail.com>
2021-02-02 19:53:51 -0700

    made roles label plural lol

--- DATCCanvasAPIApp/CoursesForm.Designer.cs
+++ DATCCanvasAPIApp/CoursesForm.Designer.cs
@@ -279,9 +279,9 @@
             this.courseRole.AutoSize = true;
             this.courseRole.Location = new System.Drawing.Point(303, 20);
             this.courseRole.Name = "courseRole";
-            this.courseRole.Size = new System.Drawing.Size(35, 13);
+            this.courseRole.Size = new System.Drawing.Size(40, 13);
             this.courseRole.TabIndex = 20;
-            this.courseRole.Text = "Role: ";
+            this.courseRole.Text = "Roles: ";
             // 
             // CoursesForm
             // 
