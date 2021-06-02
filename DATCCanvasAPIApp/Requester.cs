9f13dc3d40587409f84aa924fa298afe31383b69
Dramaege <54544044+Dramaege@users.noreply.github.com>
2021-06-02 11:00:46 -0600

    Added overload to Post to accept URL Posting

--- DATCCanvasAPIApp/Requester.cs
+++ DATCCanvasAPIApp/Requester.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;    // For StreamReader
 
 namespace CanvasAPIApp
 {
@@ -24,7 +25,7 @@ namespace CanvasAPIApp
             //I didn't want to increase it too much without knowing what might happen but I read people who set it to 20 or 50 with no problems.
             ServicePointManager.DefaultConnectionLimit = 6;
 
-        }
+        }//end Requester()
 
         public async Task<string> MakeRequestAsync(string url, string accessToken, string parameters = "")
         {
@@ -41,7 +42,8 @@ namespace CanvasAPIApp
 
             return responseString;
 
-        }
+        }//end MakeRequestAsync
+
         //Delete request created originally for concluding courses for students in Courseform
         public async Task<string> MakeDeleteRequestAsync(string url, string accessToken, string parameters = "")
         {
@@ -58,8 +60,9 @@ namespace CanvasAPIApp
 
             return responseString;
 
-        }
-        //POST method used to invite students to courses, has 4 variables
+        }//end MakeDeleteRequestAsync
+
+        //POST method, This method requires a dictionary object in the body of the POST to send the information, but also has URL post functionality
         public async Task<string> MakePOSTRequestAsync(string url, string accessToken, Dictionary<string, string> values, string parameters = "")
         {
             string finalUrl = url + $"access_token={accessToken}" + parameters;
@@ -77,6 +80,23 @@ namespace CanvasAPIApp
 
             return responseString;
 
-        }
+        }//end MakePostRequestAsync
+
+        //POST method overload, adapted to focus on only URL Post requests, allows for the exclusion of the dictionary object while keeping the async functionality
+        public async Task<string> MakePOSTRequestAsync(string url, string accessToken, string parameters = "")
+        {
+            string finalUrl = url + $"access_token={accessToken}" + parameters;
+
+            //debug output:
+            //Console.WriteLine(finalUrl);            
+
+            HttpResponseMessage response = await client.PostAsync(finalUrl, new FormUrlEncodedContent(new Dictionary<string, string> { }));
+
+            response.EnsureSuccessStatusCode();
+
+            string responseString = await response.Content.ReadAsStringAsync();
+
+            return responseString;
+        }//end MakePostRequestAsync
     }
 }
